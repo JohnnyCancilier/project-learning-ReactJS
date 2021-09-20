2be92e68a716e8877e4e215ffe9f7f0b2e808109
Johnny Cancilier <jhony.cancilier@hotmail.com>
2021-09-20 14:48:37 -0300

    att6

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -15,5 +15,5 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database;
+const auth = firebase.auth();
+const database = firebase.database();
