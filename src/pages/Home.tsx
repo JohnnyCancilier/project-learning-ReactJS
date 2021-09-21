a5aecb7501fa7156ef51d6097df3fbec3257143f
Johnny Cancilier <jhony.cancilier@hotmail.com>
2021-09-21 18:20:31 -0300

    att10

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,33 @@
 import illustrationImg from '../assets/imagens/illustration.svg';
 import logoImg from '../assets/imagens/logo.svg';
 import googleIconImg from '../assets/imagens/google-icon.svg';
-
+import '../styles/auth.scss';   
+import { Button } from '../componentes/Button';
 
 export function Home(){
     return (
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e resposatas" />
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
             <main>
-                <div>
-                    <img src={logoImg} alt="Letmeask" />
-                    <button>
+                <div className="main-content">
+                    <img src={logoImg} alt="Logo Letmeask" />
+                    <button className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
-                    <div>Ou entre em uma sala</div>
+                    <div className="separator">Ou entre em uma sala</div>
                     <form>
                         <input 
                             type="text"
                             placeholder="Digite o código da sala"
                         />
-                        <button type="submit">
+                        <Button type="submit">
                             Entrar na sala
-                        </button>
+                        </Button>
                     </form>
                 </div>
             </main>
