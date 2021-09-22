277a397caa4c54b14e3b4d0519becdde4a4e9d04
Johnny Cancilier <jhony.cancilier@hotmail.com>
2021-09-22 15:37:25 -0300

    att17

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-import {Home} from "./pages/Home";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { AuthContextProvinder } from './contexts/AuthContext'
+import { Room } from './pages/Room';
 
 function App() {
 
   return(
     <BrowserRouter>
       <AuthContextProvinder>
-        <Route  path="/" exact component={Home}/>
-        <Route  path="/rooms/new" component={NewRoom}/>
+        <Switch>
+          <Route  path="/" exact component={Home} />
+          <Route  path="/rooms/new" component={NewRoom} />
+          <Route  path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvinder>
     </BrowserRouter>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { Button } from '../componentes/Button';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 import illustrationImg from '../assets/imagens/illustration.svg';
 import logoImg from '../assets/imagens/logo.svg';
 import googleIconImg from '../assets/imagens/google-icon.svg';
@@ -10,12 +12,30 @@ import '../styles/auth.scss';
 export function Home(){
     const history = useHistory();
     const { user, singInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom(){
         if(!user){
             await singInWithGoogle()
         }
-        history.push('/rooms/new')
+        history.push('rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if(roomCode.trim() == ''){
+            return
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()){
+            alert('This room doesnt exist.');
+            return
+        }
+
+        history.push(`/rooms/${roomCode}`);
     }
 
 
@@ -34,10 +54,12 @@ export function Home(){
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">Ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
